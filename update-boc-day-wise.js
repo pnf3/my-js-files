409a23746c8e725775b1a63d09af24af71f3021c
pnf3 <167399119+pnf3@users.noreply.github.com>
2024-06-20 13:49:14 +0530

    Update update-boc-day-wise.js

--- update-boc-day-wise.js
+++ update-boc-day-wise.js
@@ -1,5 +1,5 @@
 const dayValues = {
-    "Chef Chidambara: Day-Wise Box Office Collection Worldwide": ["0.1"],
+    "Chef Chidambara: Day-Wise Box Office Collection Worldwide": ["0.09, 0.08"],
     "Love Mocktail 2 Box Office Collection Worldwide": ["200", "250", "300", "350", "400", "450", "500", "550", "600", "650", "700", "750", "800", "850"],
     // Add more titles and corresponding day values as needed
 };
