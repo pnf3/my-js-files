4296d04783ec3d1c9026c46a82e19be33a97963c
pnf3 <167399119+pnf3@users.noreply.github.com>
2025-03-02 15:47:35 +0530

    Update boc-total-day-wise-global-update.js

--- boc-total-day-wise-global-update.js
+++ boc-total-day-wise-global-update.js
@@ -1,12 +1,14 @@
- // Define cumulative box office collections for movies
-     window.dayValues = {
-        "Post Title Example": {"1": "41", "2": "47", "3": "58.5", "4": "34", "5": "35.25", "6": "42", "7": "31.5", 
-                               "8": "33.5", "9": "54", "10": "50", "11": "28", "12": "28.5", "13": "33", "14": "23.25", 
-                               "15": "14.02"},
-         
-        "Chhaava Movie Worldwide Box Office Collection Day Wise": {"1": "42", "2": "48", "3": "58.5", "4": "34", "5": "35.25", "6": "42", "7": "31.5", 
-                                                                   "8": "33.5", "9": "54", "10": "50", "11": "28", "12": "28.5", "13": "33", "14": "23.25",
-                                                                   "15": "24.15"
-                                                                   },
-"Crazxy Movie Worldwide Box Office Collection Day Wise": {"1": "1.3"}
-    };
+// Define cumulative box office collections for movies
+window.dayValues = {
+    "Post Title Example": {
+        1: 41, 2: 47, 3: 58.5, 4: 34, 5: 35.25, 6: 42, 7: 31.5,
+        8: 33.5, 9: 54, 10: 50, 11: 28, 12: 28.5, 13: 33, 14: 23.25, 15: 14.02
+    },
+    "Chhaava Movie Worldwide Box Office Collection Day Wise": {
+        1: 42, 2: 48, 3: 58.5, 4: 34, 5: 35.25, 6: 42, 7: 31.5,
+        8: 33.5, 9: 54, 10: 50, 11: 28, 12: 28.5, 13: 33, 14: 23.25, 15: 24.15
+    },
+    "Crazxy Movie Worldwide Box Office Collection Day Wise": {
+        1: 1.3
+    }
+};
