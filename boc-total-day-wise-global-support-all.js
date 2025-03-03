1bd89a24ea1fde354e659bfd00266af65e0d9194
pnf3 <167399119+pnf3@users.noreply.github.com>
2025-03-03 10:47:09 +0530

    Update boc-total-day-wise-global-support-all.js

--- boc-total-day-wise-global-support-all.js
+++ boc-total-day-wise-global-support-all.js
@@ -7,7 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
    const cumulativeValuesObj = dayValues[postTitle]; // Now an object
 
 // Convert object values to an ordered array
-const dailyEarnings = Object.values(cumulativeValuesObj).map(value => parseFloat(value.toFixed(2)));
+const dailyEarnings = Object.values(cumulativeValuesObj).map(value => Number(parseFloat(value).toFixed(2)));
+
 
     // Compute daily earnings from cumulative values
    // let previousValue = 0;
