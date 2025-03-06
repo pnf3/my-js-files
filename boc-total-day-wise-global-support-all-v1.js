1878e7fef320c37703eda09c5667f28d2078edbb
pnf3 <167399119+pnf3@users.noreply.github.com>
2025-03-06 12:24:44 +0530

    Update boc-total-day-wise-global-support-all-v1.js

--- boc-total-day-wise-global-support-all-v1.js
+++ boc-total-day-wise-global-support-all-v1.js
@@ -318,10 +318,7 @@ let todayCollectionCell, currentDayNum;
 
         totalSumElement.textContent = (totalSum + parseFloat(simulatedCollection)).toFixed(2);
         totalSumElement2.textContent = (totalSum + parseFloat(simulatedCollection)).toFixed(2);
- // Update weekSums or weekTotals with simulated collection
-    if (currentWeek) {
-        weekSums[currentWeek] += parseFloat(simulatedCollection); // Add to week sum
-        weekTotalElements[currentWeek].cells[1].textContent = weekSums[currentWeek].toFixed(2);        generateChart(); // ✅ Update the chart dynamically
+ generateChart(); // ✅ Update the chart dynamically
     }
 
     // Run immediately when the page loads
