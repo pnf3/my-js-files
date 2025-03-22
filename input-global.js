e17e11ee81b66290edc3b3bb0b8f1585f5300436
pnf3 <167399119+pnf3@users.noreply.github.com>
2025-03-22 11:57:40 +0530

    Add files via upload

---
+++ input-global.js
@@ -0,0 +1,11 @@
+<script>
+window.dayValues = {
+"Rekhachithram Box Office Collection: Day-Wise": 
+{releaseDate: "2025-01-09", 1: 2.53, 2: 4.56, 48: 57.31, total: 57.31, Rating: 9.3, Language: "Hindi", futureDays: 0},
+"The Diplomat Box Office Collection: Day-Wise": 
+{releaseDate: "2025-03-14", 1: 2.53, 2: 6.68, 5: 17.51, total: null, Rating: 9.3, Language: "Hindi", futureDays: 5},
+"COURT: State Vs A Nobody Box Office Collection: Day-Wise": 
+{releaseDate: "2025-02-14", 1: 10.12 , 2: 21.28, 5: 48.18, total: 48.18, Rating: 9.3, Language: "Telugu"},
+"Chhaava Box Office Collection: Day-Wise": 
+{releaseDate: "2025-02-14", 1: 41.12, 2: 89.28, 33: 708.18, 34: 719.23, total: null, Rating: 9.3, Language: "Hindi", Verdict: "Blockbuster", futureDays: 40}
+};</script>
