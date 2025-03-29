2c1a900246c1a370a2c51e8727b0fe5767a52f6d
pnf3 <167399119+pnf3@users.noreply.github.com>
2025-03-29 11:17:20 +0530

    Add files via upload

---
+++ input-global-v3_1.js
@@ -0,0 +1,56 @@
+<script type="application/json" id="boxOfficerunning">
+	{
+		
+		"Sikandar Box Office Collection: Day-Wise": {
+			"releaseDate": "2025-03-30", 
+			"0": 33.9,
+			"total": null, "Rating": "Comming Soon", "Language": "Hindi", "Verdict": "null", "futureDays": 30, "Budget": 200, "advanceBooking": 33.9
+		},
+		
+		"Mad Square Box Office Collection: Day-Wise": {
+			"releaseDate": "2025-03-28", 
+			"1": 4.69,
+			"total": null, "Rating": "8.4", "Language": "Telugu", "Verdict": "null", "futureDays": 30, "Budget": 15, "advanceBooking": 3.24
+		},
+		
+		"Robinhood Box Office Collection: Day-Wise": {
+			"releaseDate": "2025-03-28", 
+			"1": 4.51,
+			"total": null, "Rating": "9.0", "Language": "Telugu", "Verdict": "null", "futureDays": 30, "Budget": 60, "advanceBooking": 3.42
+		},
+		
+		
+		"L2: Empuraan Box Office Collection: Day-Wise": {
+			"releaseDate": "2025-03-27", 
+			"1": 43, "2": 76.51,
+			"total": null, "Rating": "7.9", "Language": "Malayalam", "Verdict": "null", "futureDays": 30, "Budget": 180, "advanceBooking": 34
+		},
+		
+		"The Diplomat Box Office Collection: Day-Wise": {
+			"releaseDate": "2025-03-14", 
+			"1": 2.53, "2": 6.68, "9": 30.12, 
+			"total": null, "Rating": 8.8, "Language": "Hindi", "Verdict": "null", "futureDays": 30, "Budget": 20
+		},
+		
+		"Dragon Box Office Collection: Day-Wise": {
+			"releaseDate": "2025-02-21", 
+			"1": 7.53, "2": 15.68, "28": 150.21, 
+			"total": 150.21, "Rating": 9.3, "Language": "Tamil", "Verdict": "null", "futureDays": 0, "Budget": 37
+		},
+
+		"COURT: State Vs A Nobody Box Office Collection: Day-Wise": {
+			"releaseDate": "2025-03-14", 
+			"1": 4.12 , "2": 9.28, "10": 39.51,
+			"total": null, "Rating": 9.5, "Language": "Telugu", "Verdict": "null", "futureDays": 30, "Budget": 10
+		},
+
+		"Chhaava Box Office Collection: Day-Wise": {
+			"releaseDate": "2025-02-14", 
+			"1": 41.12, "2": 89.28, "3": 140.35, "4": 185.42, "5": 225.50, "6": 260.58, "7": 292.65, 
+			"8": 320.72, "9": 346.80, "10": 370.87, "11": 392.94, "12": 413.00, "13": 431.07, "14": 447.13, 
+			"15": 462.20, "16": 476.26, "17": 489.32, "18": 501.38, "19": 512.43, "20": 522.49, "21": 531.54, 
+			"22": 539.60, "23": 546.65, "24": 552.71, "25": 557.76, "26": 561.82, "27": 564.87, "28": 792.15, 
+			"total": 792.15, "Rating": 9.3, "Language": "Hindi", "Verdict": "null", "futureDays": 0, "Budget": 130
+		}
+	}
+	</script>
