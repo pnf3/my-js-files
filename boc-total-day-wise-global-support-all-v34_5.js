523dda7e75d20f471add845bad75908a83c32a02
pnf3 <167399119+pnf3@users.noreply.github.com>
2025-04-05 20:57:24 +0530

    Update boc-total-day-wise-global-support-all-v34_5.js

--- boc-total-day-wise-global-support-all-v34_5.js
+++ boc-total-day-wise-global-support-all-v34_5.js
@@ -657,28 +657,6 @@ function animateArrow(cell) {
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    var timeElements = document.querySelectorAll("time.published"); // Select all time elements
-
-    timeElements.forEach(function(timeElement) {
-        var dateStr = timeElement.getAttribute("datetime");
-        if (dateStr) {
-            var date = new Date(dateStr);
-
-            var options = {
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-                hour: "2-digit",
-                minute: "2-digit",
-                hour12: true,
-                timeZone: "Asia/Kolkata"
-            };
-
-            timeElement.innerHTML = date.toLocaleString("en-US", options) + " IST - NF360";
-        }
-    });
-});
 
 // Related Posts Here
 document.addEventListener("DOMContentLoaded", function() {
