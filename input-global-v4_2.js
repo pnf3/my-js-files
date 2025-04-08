5234c1912d53997c367ab02de2c9eefc37b4e363
pnf3 <167399119+pnf3@users.noreply.github.com>
2025-04-08 12:26:37 +0530

    Add files via upload

---
+++ input-global-v4_2.js
@@ -0,0 +1,225 @@
+const boxOfficeData = {
+
+"Chhaava Box Office Collection: Day-Wise": {
+			"releaseDate": "2025-02-14", 
+			"1": 41.12, "2": 89.28, "3": 140.35, "4": 185.42, "5": 225.50, "6": 260.58, "7": 292.65, 
+			"8": 320.72, "9": 346.80, "10": 370.87, "11": 392.94, "12": 413.00, "13": 431.07, "15": 792.72,
+			"total": 792.15, "Rating": 9.3, "Language": "Hindi", "Verdict": "null", "futureDays": 0, "Budget": 130
+		},
+	
+	"Dragon Box Office Collection: Day-Wise": {
+			"releaseDate": "2025-02-21", 
+			"1": 7.53, "2": 15.68, "14": 150.21, 
+			"total": 150.21, "Rating": 9.3, "Language": "Tamil", "Verdict": "null", "futureDays": 0, "Budget": 37
+		},
+	
+	"Thandel Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-02-07",  
+        "total": 100.18, "Rating": 8.8, "Language": "Telugu", "Verdict": "null", "futureDays": 0, "Budget": 70
+    },
+	
+	"Vidaamuyarchi Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-02-06",  
+        "total": 138.11, "Rating": 7.6, "Language": "Tamil", "Verdict": "null", "futureDays": 0, "Budget": 200
+    },
+	
+	"Sky Force Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-24",  
+        "total": 156.31, "Rating": 8.9, "Language": "Hindi", "Verdict": "null", "futureDays": 0, "Budget": 160
+    },
+	
+	"Daaku Maharaaj Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-12",  
+        "total": 115.24, "Rating": 8.8, "Language": "Telugu", "Verdict": "null", "futureDays": 0, "Budget": 100
+    },
+	
+	"Game Changer Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-10",  
+        "total": 178.45, "Rating": 7.4, "Language": "Telugu", "Verdict": "null", "futureDays": 0, "Budget": 385
+    },
+	
+	"Rekhachithram Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-09", 
+        "1": 2.53, "2": 7.09, "3": 12.65, "4": 17.89, "5": 21.34, "6": 22.92, "7": 24.50, 
+        "8": 26.80, "9": 29.95, "10": 32.75, "11": 34.90, "12": 36.85, "13": 38.65, "15": 83.08,
+        "total": 83.08, "Rating": 8.8, "Language": "Malayalam", "Verdict": "null", "futureDays": 0, "Budget": 8
+    },
+	
+	"Sankranthiki Vasthunam Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-09",  
+        "total": 302.92, "Rating": 8.9, "Language": "Telugu", "Verdict": "null", "futureDays": 0, "Budget": 60
+    },
+	
+	"Madha Gaja Raja Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-12",  
+        "total": 56.20, "Rating": 8.0, "Language": "Tamil", "Verdict": "null", "futureDays": 0, "Budget": 15
+    },
+	
+	"Deva Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-31",  
+        "total": 55.37, "Rating": 7.5, "Language": "Hindi", "Verdict": "null", "futureDays": 0, "Budget": 52
+    },
+	
+	"Officer on Duty Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-02-20",  
+        "total": 50.17, "Rating": 9.1, "Language": "Malayalam", "Verdict": "null", "futureDays": 0, "Budget": 10
+    },
+	
+	"Kudumbasthan Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-24",  
+        "total": 28.42, "Rating": 9.1, "Language": "Tamil", "Verdict": "null", "futureDays": 0, "Budget": 10
+    },
+	
+	"Emergency Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-17",  
+        "total": 21.88, "Rating": 8.0, "Language": "Hindi", "Verdict": "null", "futureDays": 0, "Budget": 60
+    },
+	
+	"Dominic and the Ladies' Purse Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-23",  
+        "total": 18.01, "Rating": 8.0, "Language": "Malayalam", "Verdict": "null", "futureDays": 0, "Budget": 19
+    },
+	
+	"Identity Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-02",  
+        "total": 18.21, "Rating": 7.9, "Language": "Malayalam", "Verdict": "null", "futureDays": 0, "Budget": 12
+    },
+	
+	"Fateh Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-10",  
+        "total": 16.25, "Rating": 8.1, "Language": "Hindi", "Verdict": "null", "futureDays": 0, "Budget": 30
+    },
+	
+	"Kadhalikka Neramillai Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-14",  
+        "total": 16.51, "Rating": 7.5, "Language": "Tamil", "Verdict": "null", "futureDays": 0, "Budget": 10
+    },
+	
+	"Bromance Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-14",  
+        "total": 12.17, "Rating": 7.7, "Language": "Malayalam", "Verdict": "null", "futureDays": 0, "Budget": 8
+    },
+	
+	"Mere Husband Ki Biwi Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-02-21",  
+        "total": 12.73, "Rating": 6.9, "Language": "Hindi", "Verdict": "null", "futureDays": 0, "Budget": 60
+    },
+	
+	"Vanangaan Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-10",  
+        "total": 13.38, "Rating": 8.0, "Language": "Tamil", "Verdict": "null", "futureDays": 0, "Budget": 50
+    },
+	
+	"Nilavuku En Mel Ennadi Kobam Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-02-21",  
+        "total": 10.22, "Rating": 7.8, "Language": "Tamil", "Verdict": "null", "futureDays": 0, "Budget": 15
+    },
+	
+	"Azaad Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-17",  
+        "total": 9.58, "Rating": 6.9, "Language": "Hindi", "Verdict": "null", "futureDays": 0, "Budget": 80
+    },
+	
+	"Mazaka Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-02-26",
+		"1": 1.53, "2": 2.09, "7": 9.19,	
+        "total": 9.19, "Rating": 8.5, "Language": "Telugu", "Verdict": "null", "futureDays": 0, "Budget": 25
+    },
+	
+	"Ponman Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-30",
+        "total": 9.79, "Rating": 8.7, "Language": "Malayalam", "Verdict": "null", "futureDays": 0, "Budget": 3
+    },
+	
+	"Loveyapa Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-02-07",
+        "total": 12.49, "Rating": 7.9, "Language": "Hindi", "Verdict": "null", "futureDays": 0, "Budget": 60
+    },
+	
+	"Oru Jaathi Jathakam Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-31",
+        "total": 7.23, "Rating": 7.8, "Language": "Malayalam", "Verdict": "null", "futureDays": 0, "Budget": 5
+    },
+	
+	"Choo Mantar Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-10",
+        "total": 6.21, "Rating": 9.1, "Language": "Kannada", "Verdict": "null", "futureDays": 0, "Budget": 8
+    },
+	
+	"Pravinkoodu Shappu Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-16",
+        "total": 5.19, "Rating": 7.4, "Language": "Malayalam", "Verdict": "null", "futureDays": 0, "Budget": 10
+    },
+	
+	"Kingston Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-03-07",
+		"1": 1.03, "2": 1.59, "7": 5.35,
+        "total": 5.35, "Rating": 7.3, "Language": "Tamil", "Verdict": "null", "futureDays": 0, "Budget": 20
+    },
+	
+	"Sabdham Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-02-28",
+		"1": 1.01, "2": 2.18, "8": 11.16,
+        "total": 11.18, "Rating": 8.0, "Language": "Tamil", "Verdict": "null", "futureDays": 0, "Budget": 15
+    },
+	
+	"Ennu Swantham Punyalan Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-10",
+        "total": 2.65, "Rating": 8.1, "Language": "Malayalam", "Verdict": "null", "futureDays": 0, "Budget": 9
+    },
+	
+	"Aghathiyaa Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-02-28",
+        "total": 2.15, "Rating": 6.5, "Language": "Tamil", "Verdict": "null", "futureDays": 0, "Budget": 22
+    },
+	
+	"Forest Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-24",
+        "total": 1.40, "Rating": 9.0, "Language": "Kannada", "Verdict": "null", "futureDays": 0, "Budget": 1
+    },
+	
+	"Royal Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-24",
+        "total": 1.07, "Rating": 8.1, "Language": "Kannada", "Verdict": "null", "futureDays": 0, "Budget": 1
+    },
+	
+	"Sanju Weds Geetha 2 Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-17",
+        "total": 0.88, "Rating": 8.9, "Language": "Kannada", "Verdict": "null", "futureDays": 0, "Budget": 0.45
+    },
+	
+	"Umbarro Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-24",
+        "total": 13.31, "Rating": 9.1, "Language": "Gujarati", "Verdict": "null", "futureDays": 0, "Budget": 4
+    },
+	
+	"Faati Ne? Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-31",
+        "total": 3.25, "Rating": 9.2, "Language": "Gujarati", "Verdict": "null", "futureDays": 0, "Budget": 2.25
+    },
+	
+	"Mom Tane Nai Samjay Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-10",
+        "total": 3.18, "Rating": 8.3, "Language": "Gujarati", "Verdict": "null", "futureDays": 0, "Budget": 2.58
+    },
+	
+	"All The Best Pandya Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-03-14",
+        "total": 2.81, "Rating": 9.1, "Language": "Gujarati", "Verdict": "null", "futureDays": 0, "Budget": 1
+    },
+	
+	"Kaashi Raaghav Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-03",
+        "total": 1.35, "Rating": 8.0, "Language": "Gujarati", "Verdict": "null", "futureDays": 0, "Budget": 0.40
+    },
+	
+	"Fussclass Dabhade Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-24",
+        "total": 8.67, "Rating": 8.6, "Language": "Marathi", "Verdict": "null", "futureDays": 0, "Budget": 5.62
+    },
+	
+	"Sangeet Manapmaan Box Office Collection: Day-Wise": {
+        "releaseDate": "2025-01-10",
+        "total": 2.31, "Rating": 8.7, "Language": "Marathi", "Verdict": "null", "futureDays": 0, "Budget": 8
+    }
+}
