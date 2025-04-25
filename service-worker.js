1bb9a24c2258aa47eeeac52e69544e2cab26c31e
pnf3 <167399119+pnf3@users.noreply.github.com>
2025-04-25 20:19:44 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'nf360-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://www.newsfocus360.com/'
+];
+
+// Install
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
