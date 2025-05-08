b46ad578a4266bf2ae62fb2485a2aa83aaf1dde2
pnf3 <167399119+pnf3@users.noreply.github.com>
2025-05-08 16:04:11 +0530

    Add files via upload

---
+++ moviename_internal_linking_1.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', function() {
+    const movieLinks = {
+        "Housefull 5": "https://www.newsfocus360.com/2025/05/housefull-5-box-office-collection-day.html",
+        "Bhool Chuk Maaf": "https://www.newsfocus360.com/2025/05/bhool-chuk-maaf-box-office-collection.html"
+        
+    };
+
+    function linkifyElement(el, linksObj) {
+        let html = el.innerHTML;
+
+        const sortedMovies = Object.keys(linksObj).sort((a, b) => b.length - a.length);
+
+        sortedMovies.forEach(movie => {
+            const link = linksObj[movie];
+            const escapedMovie = movie.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
+            const regex = new RegExp(`\\b(${escapedMovie})\\b`, 'g');
+            html = html.replace(regex, `<a href="${link}" target="_blank">$1</a>`);
+        });
+
+        el.innerHTML = html;
+    }
+
+    // 🔍 Select all elements with the .custom-table class
+    const targets = document.querySelectorAll('.custom-table');
+
+    targets.forEach(el => linkifyElement(el, movieLinks));
+});
---
+++ name_internal_linking_1.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', function() {
+    const nameLinks = {
+       "Akshay Kumar": "https://www.newsfocus360.com/2023/10/akshay-kumar-movies-list.html",
+        "Nani": "https://www.newsfocus360.com/2018/12/nani-all-movies-list-and-biography-hits.html"
+    };
+
+    function linkifyElement(el, linksObj) {
+        let html = el.innerHTML;
+
+        const sortedNames = Object.keys(linksObj).sort((a, b) => b.length - a.length);
+
+        sortedNames.forEach(name => {
+            const link = linksObj[name];
+            const escapedName = name.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
+            const regex = new RegExp(`\\b(${escapedName})\\b`, 'g');
+            html = html.replace(regex, `<a href="${link}" target="_blank">👤 $1</a>`);
+        });
+
+        el.innerHTML = html;
+    }
+
+    // 🔍 Select both required divs by their classes
+    const targets = document.querySelectorAll('.box-office-cast, .box-office-crew');
+
+    targets.forEach(el => linkifyElement(el, nameLinks));
+});
