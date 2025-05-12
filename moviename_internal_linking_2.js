caee5c6df76249d5f1e04c46370a37673f9cf5e9
pnf3 <167399119+pnf3@users.noreply.github.com>
2025-05-12 10:58:47 +0530

    Add files via upload

---
+++ moviename_internal_linking_2.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', function() {
+    const movieLinks = {
+        "Housefull 5": "https://www.newsfocus360.com/2025/05/housefull-5-box-office-collection-day.html",
+        "Bhool Chuk Maaf": "https://www.newsfocus360.com/2025/05/bhool-chuk-maaf-box-office-collection.html"
+    };
+
+    function linkifyElement(el, linksObj) {
+        let html = el.innerHTML;
+
+        const sortedMovies = Object.keys(linksObj).sort((a, b) => b.length - a.length);
+
+        sortedMovies.forEach(movie => {
+            const link = linksObj[movie];
+            const escapedMovie = movie.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
+            const regex = new RegExp(`\\b(${escapedMovie})\\b`, 'g');
+            html = html.replace(regex, `<a href="${link}" target="_blank">$1</a>`);
+        });
+
+        el.innerHTML = html;
+    }
+
+    // Select all elements with the .post-sub-body class
+    const targets = document.querySelectorAll('.post-sub-body');
+
+    targets.forEach(el => {
+        // Skip elements with the .box-office-container class
+        if (!el.classList.contains('box-office-container')) {
+            linkifyElement(el, movieLinks);
+        }
+    });
+});
