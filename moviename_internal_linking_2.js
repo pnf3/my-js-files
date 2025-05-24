ec44318f87094de6a0c57eaccde48b8a7786bf46
pnf3 <167399119+pnf3@users.noreply.github.com>
2025-05-24 14:09:34 +0530

    Update moviename_internal_linking_2.js

--- moviename_internal_linking_2.js
+++ moviename_internal_linking_2.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function() {
 	    "Bombay": "https://www.newsfocus360.com/2025/05/bombay-box-office-collection-day-wise.html",
 "Bhairavam":"https://www.newsfocus360.com/2025/05/bhairavam-box-office-collection-day-wise.html",
         "Housefull 5": "https://www.newsfocus360.com/2025/05/housefull-5-box-office-collection-day.html",
-        "Bhool Chuk Maaf": "https://www.newsfocus360.com/2025/05/bhool-chuk-maaf-box-office-collection.html",
+        "Bhool Chuk Maaf": "https://www.newsfocus360.com/2025/05/bhool-chuk-maaf-box-office-collection_23.html",
 		"The Shawshank Redemption": "https://www.newsfocus360.com/2025/05/the-shawshank-redemption-timeless-tale.html"
     };
 
@@ -16,14 +16,14 @@ window.addEventListener('load', function() {
             const link = linksObj[movie];
             const escapedMovie = movie.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
             const regex = new RegExp(`\\b(${escapedMovie})\\b`, 'g');
-            html = html.replace(regex, `<a href="${link}" target="_blank">$1</a>`);
+            html = html.replace(regex, `<a href="${link}">$1</a>`);
         });
 
         el.innerHTML = html;
     }
 
     // Select all elements with the .post-sub-body class
-    const targets = document.querySelectorAll('.post-sub-body');
+    const targets = document.querySelectorAll('.post-sub-body, .post-boc-body');
 
     targets.forEach(el => {
         // Skip elements with the .box-office-container class
