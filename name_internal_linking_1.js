66890aa1ff9284136fd600ccb381ffd98ccddb0b
pnf3 <167399119+pnf3@users.noreply.github.com>
2025-05-24 13:44:01 +0530

    Update name_internal_linking_1.js

--- name_internal_linking_1.js
+++ name_internal_linking_1.js
@@ -1,5 +1,12 @@
 window.addEventListener('load', function() {
     const nameLinks = {
+        "Gavie Chahal": "https://www.newsfocus360.com/2025/05/gavie-chahal-movies-list.html",
+"Sanjay Niranjan":"https://www.newsfocus360.com/2025/05/sanjay-niranjan-movies-list.html",
+"Deepshikha Nagpal":"https://www.newsfocus360.com/2025/05/deepshikha-nagpal-movies-list.html",
+"Bellamkonda Sai Srinivas":"https://www.newsfocus360.com/2025/05/bellamkonda-sai-srinivas-movies-list.html",
+"Manchu Manoj":"https://www.newsfocus360.com/2025/05/manchu-manoj-movies-list.html",
+"Nara Rohith":"https://www.newsfocus360.com/2025/05/nara-rohith-movies-list.html",
+"Vijay Kanakamedala":"https://www.newsfocus360.com/2025/05/vijay-kanakamedala-movies-list.html",
        "Akshay Kumar": "https://www.newsfocus360.com/2023/10/akshay-kumar-movies-list.html",
         "Hrithik Roshan": "https://www.newsfocus360.com/2024/01/hrithik-roshan-all-movies-list-year-wise.html",
         "Nani": "https://www.newsfocus360.com/2018/12/nani-all-movies-list-and-biography-hits.html"
