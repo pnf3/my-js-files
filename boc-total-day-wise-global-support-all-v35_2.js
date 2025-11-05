c95895a0e4c1928c737087cdcbf69e9f62f9ebac
pnf3 <167399119+pnf3@users.noreply.github.com>
2025-11-05 11:59:52 +0530

    Update boc-total-day-wise-global-support-all-v35_2.js

--- boc-total-day-wise-global-support-all-v35_2.js
+++ boc-total-day-wise-global-support-all-v35_2.js
@@ -436,11 +436,11 @@ document.addEventListener("DOMContentLoaded", function() {
     if (metaDescription) {
         if (entryTitle.textContent.includes("Box Office Collection")) {
             metaDescription.content = `${movieName} - Track worldwide earnings, daily updates, budget, cast, crew, and total gross up to ${latestDay}. Stay tuned for the latest box office performance and verdict.`;
-              document.title = `${movieName}  ${latestDay} Box Office Collection`;
+              document.title = `${movieName} From Day 1 to ${latestDay} Box Office Collection`;
         } else {
            // metaDescription.content = `Explore the complete list of ${entryTitle.textContent.trim()} List year-wise, from the first film to the latest and upcoming releases, along with the total movie count.`;
         }
-        entryTitle.textContent = entryTitle.textContent.replace("Box Office Collection: Day-Wise", `Day 1 to ${latestDay} Box Office Collection`);
+        entryTitle.textContent = entryTitle.textContent.replace("Box Office Collection: Day-Wise", `From Day 1 to ${latestDay} Box Office Collection`);
 const latestDay2 = document.getElementById("latest-day2");
 latestDay2.textContent = latestDay2.textContent.replace("Day-Wise", `From Day 1 to ${latestDay}`);
 
@@ -803,6 +803,7 @@ row.appendChild(releaseDateCell); // Append at the end
     tableBody.innerHTML = ""; // Clear existing rows
     rowsData.forEach(({ row }) => tableBody.appendChild(row));
 }); 
+
 
 
 
